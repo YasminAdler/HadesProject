22feffd089f6b3fcefa7148fbf06d13f8e259ef4
YasminAdler <adleryasmin@gmail.com>
2022-12-25 14:09:29 +0200

    done

--- map.cpp
+++ map.cpp
@@ -8,7 +8,7 @@ inline Map::Map()
       rooms(NULL),
       numbeOfRooms(0),
       currentRoon(NULL),
-      Start(NULL){}      
+      Start(NULL){}  ;    
 
 /* Functions */
 Room *Map::AddRoom(Room, Direction)
